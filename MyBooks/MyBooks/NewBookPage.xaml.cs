a283fe576e356c194f3adb943fdbcc58e35194ec
jameswhitakerwork <jameswhitakerwork@gmail.com>
2018-02-26 17:00:22 +0900

    Add project files.

---
+++ MyBooks/MyBooks/NewBookPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MyBooks
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class NewBookPage : ContentPage
+	{
+		public NewBookPage ()
+		{
+			InitializeComponent ();
+		}
+
+    private void Button_Clicked(object sender, EventArgs e)
+        {
+            Book book = new Book()
+            {
+                Name = nameEntry.Text,
+                Author = authorEntry.Text
+            };
+
+            using (SQLite.SQLiteConnection conn = new SQLite.SQLiteConnection(App.DB_PATH))
+            {
+                conn.CreateTable<Book>();
+                var numberOfRows = conn.Insert(book);
+
+                if (numberOfRows > 0)
+                {
+                    DisplayAlert("Success!", "Book inserted", "Great");
+                }
+                else
+                    DisplayAlert("Failure", "Book failed to be inserted", "Damn it");
+            }
+                
+
+
+           
+        }
+	}
+}
